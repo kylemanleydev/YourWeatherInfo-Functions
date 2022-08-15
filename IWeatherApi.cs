d4dede84fa2fa3f7fac4ba5913d47641b36f167e
kmanley <kyle.manley@contracts365.com>
2022-08-15 11:31:24 -0400

    Added comment regarding configuration

--- IWeatherApi.cs
+++ IWeatherApi.cs
@@ -1,10 +1,12 @@
 ﻿using RestEase;
 using System.Threading.Tasks;
+using System;
 
 namespace YourWeatherInfo_Functions
 {
     public interface IWeatherApi
     {
+        //set api key from weatherapi.com as the key query string parameter in URL below
         [Get("v1/current.json?key=5e8bd5b4258b4f05a6111158220707&q={zipcode}&aqi=no")]
         Task<WeatherData> GetWeatherDataAsync([Path] string zipcode);
     }
